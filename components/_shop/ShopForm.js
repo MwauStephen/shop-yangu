4a734287fd548a72d356b14bf677be90c8abc1a6
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 09:48:50 +0300

    created shop form for adding new shop

--- components/_shop/ShopForm.js
+++ components/_shop/ShopForm.js
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import { Box, Card, Flex, Input, Text, Textarea } from "@chakra-ui/react";
+import { Field } from "../ui/field";
+import {
+  FileUploadList,
+  FileUploadRoot,
+  FileUploadTrigger,
+} from "../ui/file-upload";
+import { Button } from "../ui/button";
+import { HiUpload } from "react-icons/hi";
 
 const ShopForm = () => {
-  return <div>ShopForm</div>;
+  return (
+    <Box>
+      {/* <Text fontSize="1.2rem" fontWeight="bold" mb={4}>
+        Add Shop
+      </Text> */}
+      <Flex as="form" flexDirection={{ base: "column", md: "column" }} gap={4}>
+        <Field label="Shop Name" required>
+          <Input placeholder="name of shop" />
+        </Field>
+        <Field label="Shop Description" required>
+          <Textarea placeholder="Your tech paradise" />
+        </Field>
+        <Field required label="Shop Logo">
+          <UploadButton />
+        </Field>
+        <Box textAlign={{ base: "center", md: "right" }}>
+          <Button textAlign="left" variant="outline" type="reset" mr={4}>
+            Cancel
+          </Button>
+          <Button
+            textAlign="left"
+            bgGradient="to-r"
+            gradientFrom="#0da487"
+            gradientTo="#20c997"
+          >
+            Submit
+          </Button>
+        </Box>
+      </Flex>
+      {/* </Card.Root> */}
+    </Box>
+  );
 };
 
 export default ShopForm;
+
+const UploadButton = () => {
+  return (
+    <FileUploadRoot>
+      <FileUploadTrigger asChild>
+        <Button variant="outline" size="sm">
+          <HiUpload /> Upload file
+        </Button>
+      </FileUploadTrigger>
+      <FileUploadList />
+    </FileUploadRoot>
+  );
+};
