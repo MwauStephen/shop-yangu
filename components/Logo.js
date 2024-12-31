561b8038c80687dad1d5134de3e290b26bbcef61
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 12:14:30 +0300

    passed down some props to logo

--- components/Logo.js
+++ components/Logo.js
@@ -2,20 +2,20 @@ import React from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import { Avatar } from "./ui/avatar";
 
-const Logo = () => {
+const Logo = ({ isCollapsed }) => {
   return (
     <Flex flexDirection="column" alignItems="center" justifyContent="center">
       <Avatar
         name="shop-yangu"
         src="/shop-2.png"
         size="lg"
-        boxSize="50px"
-        // shape="full"
-        // border="2px solid #ff4500"
+        boxSize={isCollapsed ? "30px" : "60px"}
       />
-      <Text as="h2" fontWeight="bold">
-        Shop Yangu
-      </Text>
+      {!isCollapsed && (
+        <Text as="h2" fontWeight="bold" color="#fff" mt={2}>
+          Shop Yangu
+        </Text>
+      )}
     </Flex>
   );
 };
