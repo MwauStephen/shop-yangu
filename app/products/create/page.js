e68b116e9751af92aad60fee9c0012e02bde3034
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 12:15:35 +0300

    added product form and shop form

--- app/products/create/page.js
+++ app/products/create/page.js
@@ -1,7 +1,16 @@
+import ProductForm from "@/components/_product/ProductForm";
+import { Card, Text } from "@chakra-ui/react";
 import React from "react";
 
 const CreateProductPage = () => {
-  return <div>product page</div>;
+  return (
+    <Card.Root p="2rem 1rem" w={{ base: "100%", md: "50%" }} mx="auto" my={8}>
+      <Text as="h1" fontWeight="bold" fontSize="1.2rem" mb={8}>
+        Add New Product
+      </Text>
+      <ProductForm />
+    </Card.Root>
+  );
 };
 
 export default CreateProductPage;
--- app/shop/create/page.js
+++ app/shop/create/page.js
@@ -1,7 +1,16 @@
+import ShopForm from "@/components/_shop/ShopForm";
+import { Card, Text } from "@chakra-ui/react";
 import React from "react";
 
 const CreateShopPage = () => {
-  return <div> create shop page</div>;
+  return (
+    <Card.Root p="2rem 1rem" w={{ base: "100%", md: "50%" }} mx="auto" my={8}>
+      <Text as="h1" fontWeight="bold" fontSize="1.2rem" mb={8}>
+        Add New Shop
+      </Text>
+      <ShopForm />
+    </Card.Root>
+  );
 };
 
 export default CreateShopPage;
