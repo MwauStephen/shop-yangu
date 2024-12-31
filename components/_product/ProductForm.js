1f18df99fceb6a2a63971680f13f13427c686051
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 10:15:00 +0300

    created product form

--- components/_product/ProductForm.js
+++ components/_product/ProductForm.js
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import { Box, Card, Flex, Input, Text, Textarea } from "@chakra-ui/react";
+import { Field } from "../ui/field";
+import { Button } from "../ui/button";
+import UploadButton from "../UploadButton";
 
 const ProductForm = () => {
-  return <div>ProductForm</div>;
+  return (
+    <Box>
+      <Flex as="form" flexDirection={{ base: "column", md: "column" }} gap={4}>
+        <Field label="Product Name" required>
+          <Input placeholder="name of product" />
+        </Field>
+        <Field label="Product Price" required>
+          <Input placeholder="Ksh 100" />
+        </Field>
+        <Field label="Stock Level" required>
+          <Input placeholder="30" />
+        </Field>
+        <Field label="Product Description" required>
+          <Textarea placeholder="Latest model with high-end features" />
+        </Field>
+        <Field required label="Product Image">
+          <UploadButton />
+        </Field>
+        <Box textAlign={{ base: "center", md: "right" }}>
+          <Button textAlign="left" variant="outline" type="reset" mr={4}>
+            Cancel
+          </Button>
+          <Button
+            textAlign="left"
+            bgGradient="to-r"
+            gradientFrom="#0da487"
+            gradientTo="#20c997"
+          >
+            Submit
+          </Button>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default ProductForm;
