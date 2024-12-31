46c35f2f83577d7f115f02a4aefd89f97bbc8b45
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 09:48:18 +0300

    created shop table and ensured shop actions ui work

--- components/_shop/ShopList.js
+++ components/_shop/ShopList.js
@@ -1,60 +1,199 @@
-import { Card, Table, Text, IconButton, Flex } from "@chakra-ui/react";
+import {
+  Card,
+  Table,
+  Text,
+  IconButton,
+  Flex,
+  Button,
+  Box,
+  Stack,
+  Badge,
+} from "@chakra-ui/react";
+
 import { FaEye, FaPencil, FaTrash } from "react-icons/fa6";
 import Pagination from "../Pagination";
+import ViewCard from "../ViewCard";
+import Image from "next/image";
+import ShopForm from "./ShopForm";
 
 const items = [
-  { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
-  { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-  { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
-  { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
-  { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
+  {
+    id: 1,
+    name: "Fashion Fiesta",
+    description: "Your tech paradise.",
+    status: "Active",
+    logo: "/shop-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Fashion Fiesta",
+    description: "Your tech paradise.",
+    status: "Active",
+    logo: "/shop-1.jpg",
+  },
+  {
+    id: 3,
+    name: "Fashion Fiesta",
+    description: "Your tech paradise.",
+    status: "Active",
+    logo: "/shop-1.jpg",
+  },
+  {
+    id: 4,
+    name: "Fashion Fiesta",
+    description: "Your tech paradise.",
+    status: "Active",
+    logo: "/shop-1.jpg",
+  },
+  {
+    id: 5,
+    name: "Fashion Fiesta",
+    description:
+      "Your tech paradise.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    status: "Active",
+    logo: "/shop-1.jpg",
+  },
 ];
 
 const ShopList = () => {
   return (
     <Card.Root p={4} boxShadow="xl">
-      <Table.Root size="sm" striped>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>Shop No.</Table.ColumnHeader>
-            <Table.ColumnHeader>Shop Name</Table.ColumnHeader>
-            <Table.ColumnHeader>Shop Description</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">Shop Status</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">Shop Logo</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">
-              Shop Actions
-            </Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {items.map((item, index) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{index + 1}</Table.Cell>
-              <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>{item.category}</Table.Cell>
-              <Table.Cell textAlign="end">{item.price}</Table.Cell>
-              <Table.Cell textAlign="end">{item.price}</Table.Cell>
-              <Table.Cell textAlign="end">
-                <Flex justifyContent="space-around">
-                  <IconButton size="xs" background="teal">
-                    <FaEye size="1px" />
-                  </IconButton>
+      <Box textAlign="end" mb={4}>
+        <ViewCard
+          triggerContent={
+            <Button
+              bgGradient="to-r"
+              gradientFrom="#0da487"
+              gradientTo="#20c997"
+            >
+              Add New
+            </Button>
+          }
+          title="Add New Shop"
+        >
+          <ShopForm />
+        </ViewCard>
+      </Box>
 
-                  <IconButton size="xs" background="teal">
-                    <FaPencil size="1px" />
-                  </IconButton>
-                  <IconButton size="xs" background="teal">
-                    <FaTrash size="1px" />
-                  </IconButton>
-                </Flex>
-              </Table.Cell>
+      <Table.ScrollArea>
+        <Table.Root size="sm" striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Shop No.</Table.ColumnHeader>
+              <Table.ColumnHeader>Shop Name</Table.ColumnHeader>
+              <Table.ColumnHeader>Shop Description</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="end">
+                Shop Status
+              </Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="end">Shop Logo</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="end">
+                Shop Actions
+              </Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-        <Table.Footer>
-          <Pagination/>
-        </Table.Footer>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {items.map((item, index) => (
+              <Table.Row key={item.id}>
+                <Table.Cell>{index + 1}</Table.Cell>
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell
+                  maxWidth="300px"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                >
+                  {item.description}
+                </Table.Cell>
+                <Table.Cell textAlign="end">
+                  <Badge colorPalette="green">{item.status}</Badge>
+                </Table.Cell>
+                <Table.Cell>
+                  <Flex justifyContent="flex-end">
+                    <Image
+                      src={item.logo}
+                      width={40}
+                      height={40}
+                      alt={item.name}
+                    />
+                  </Flex>
+                </Table.Cell>
+                <Table.Cell textAlign="end">
+                  <Flex justifyContent="space-around">
+                    <ViewCard
+                      triggerContent={
+                        <IconButton background="teal" size="2xs">
+                          <FaEye />
+                        </IconButton>
+                      }
+                      title="Shop Details"
+                      footerActions={[
+                        <Button key="close" variant="outline">
+                          Close
+                        </Button>,
+                      ]}
+                    >
+                      <Stack spacing={4}>
+                        <Text>
+                          <Image
+                            src={item.logo}
+                            width={400}
+                            height={400}
+                            alt={item.name}
+                          />
+                        </Text>
+                        <Text>
+                          <b>Shop Name:</b> {item.name}
+                        </Text>
+                        <Text>
+                          <b>Description: </b> {item.description}
+                        </Text>
+                      </Stack>
+                    </ViewCard>
+
+                    <ViewCard
+                      triggerContent={
+                        <IconButton size="2xs" background="blue.500">
+                          <FaPencil size="1px" />
+                        </IconButton>
+                      }
+                      title={`Edit ${item.name}`}
+                    >
+                      <ShopForm />
+                    </ViewCard>
+
+                    <ViewCard
+                      triggerContent={
+                        <IconButton size="2xs" background="red.600">
+                          <FaTrash size="1px" />
+                        </IconButton>
+                      }
+                      title={`Delete ${item.name}`}
+                      footerActions={[
+                        <Flex gap={2} key={item.id}>
+                          <Button key="close" variant="outline">
+                            Close
+                          </Button>
+                          <Button key="close" colorPalette="red">
+                            Delete
+                          </Button>
+                        </Flex>,
+                      ]}
+                    >
+                      <Text>
+                        Are you sure you want to delete {` ${item.name}`}?This
+                        action cannot be undone.
+                      </Text>
+                    </ViewCard>
+                  </Flex>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+          <Table.Footer>
+            <Pagination />
+          </Table.Footer>
+        </Table.Root>
+      </Table.ScrollArea>
     </Card.Root>
   );
 };
