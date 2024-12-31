fb473f8ad0424c3de60370a32c126812a3836da2
MwauStephen <stephenmwau41@gmail.com>
2024-12-31 10:14:42 +0300

    added some ui changes

--- components/_shop/ShopForm.js
+++ components/_shop/ShopForm.js
@@ -1,20 +1,12 @@
 "use client";
 import { Box, Card, Flex, Input, Text, Textarea } from "@chakra-ui/react";
 import { Field } from "../ui/field";
-import {
-  FileUploadList,
-  FileUploadRoot,
-  FileUploadTrigger,
-} from "../ui/file-upload";
+import UploadButton from "../UploadButton";
 import { Button } from "../ui/button";
-import { HiUpload } from "react-icons/hi";
 
 const ShopForm = () => {
   return (
     <Box>
-      {/* <Text fontSize="1.2rem" fontWeight="bold" mb={4}>
-        Add Shop
-      </Text> */}
       <Flex as="form" flexDirection={{ base: "column", md: "column" }} gap={4}>
         <Field label="Shop Name" required>
           <Input placeholder="name of shop" />
@@ -45,16 +37,3 @@ const ShopForm = () => {
 };
 
 export default ShopForm;
-
-const UploadButton = () => {
-  return (
-    <FileUploadRoot>
-      <FileUploadTrigger asChild>
-        <Button variant="outline" size="sm">
-          <HiUpload /> Upload file
-        </Button>
-      </FileUploadTrigger>
-      <FileUploadList />
-    </FileUploadRoot>
-  );
-};
