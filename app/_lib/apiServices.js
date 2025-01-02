b30c00bd60663b9751b75195c1bc1553ce861f54
MwauStephen <stephenmwau41@gmail.com>
2025-01-02 09:35:55 +0300

    created api functions for CRUD shop operation

--- app/_lib/apiServices.js
+++ app/_lib/apiServices.js
@@ -1,1 +1,46 @@
 // a file for all api requests to the json server
+
+// API request to get all shops
+export const getAllShops = async () => {
+  const res = await fetch("http://localhost:8001/shops");
+  const data = await res.json();
+  return data;
+};
+
+// api function to add a new shop
+export const addShop = async (shop) => {
+  const res = await fetch("http://localhost:8001/shops", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(shop),
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+// api function to update a shop
+export const updateShop = async (shop) => {
+  const res = await fetch(`http://localhost:8001/shops/${shop.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(shop),
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+// api function to delete a shop
+export const deleteShop = async (id) => {
+  const res = await fetch(`http://localhost:8001/shops/${id}`, {
+    method: "DELETE",
+  });
+
+  const data = await res.json();
+  return data;
+};
