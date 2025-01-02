1fe44e841871bf7908c4e6fb6684379ac5c31b05
MwauStephen <stephenmwau41@gmail.com>
2025-01-02 12:19:01 +0300

    created a dump file to hold unused or commented out code

---
+++ dump.js
@@ -0,0 +1,12 @@
+// const UploadButton = ({ ...rest }) => {
+//   return (
+//     <FileUploadRoot>
+//       <FileUploadTrigger asChild>
+//         <Button variant="outline" size="sm" {...rest}>
+//           <HiUpload /> Upload file
+//         </Button>
+//       </FileUploadTrigger>
+//       <FileUploadList />
+//     </FileUploadRoot>
+//   );
+// };
