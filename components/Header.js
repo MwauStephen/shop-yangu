c847fb955d899d39c182724ea96f709faa7008a9
MwauStephen <stephenmwau41@gmail.com>
2025-01-02 07:28:12 +0300

    finished creating header component layout

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { InputGroup } from "./ui/input-group";
+import { FaSearchengin } from "react-icons/fa6";
+import { Avatar } from "./ui/avatar";
 
 const Header = () => {
   return (
-    <Box as="header" bg="yellow" p="4">
-      Header
+    <Box as="header" bg="#fff" p="4" boxShadow="md">
+      <Flex alignItems="center" justifyContent="space-between">
+        <Text as="h1" fontSize="1.2rem" fontWeight="bold">
+          Shop Yangu
+        </Text>
+        <Box>
+          <InputGroup startElement={<FaSearchengin />} flex="1" w="30rem">
+            <Input placeholder="Username" />
+          </InputGroup>
+        </Box>
+        <Box>
+          <Avatar />
+        </Box>
+      </Flex>
     </Box>
   );
 };
