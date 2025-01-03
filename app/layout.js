cc476c60e65eb7140d4e69c17222d5425da9fd6a
MwauStephen <stephenmwau41@gmail.com>
2025-01-04 01:34:43 +0300

    included query provider

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Provider } from "@/components/ui/provider";
 import DashboardLayout from "@/components/DashboardLayout";
 import { Roboto } from "next/font/google";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import QueryProvider from "@/components/QueryProvider";
 
 // loading fonts
 const roboto = Roboto({
@@ -16,21 +15,13 @@ export const metadata = {
   title: "Shop Yangu | E-commerce",
   description: "An E-commerce admin dashboard",
 };
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 0,
-    },
-  },
-});
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className={roboto.className}>
         <Provider>
-          <QueryClientProvider client={queryClient} />
-          <ReactQueryDevtools initialIsOpen={false} />
+          <QueryProvider />
           <DashboardLayout>{children}</DashboardLayout>
         </Provider>
       </body>
