7707af0d5f4af3e76adba8370d1ab92a5f83e4f1
MwauStephen <stephenmwau41@gmail.com>
2025-01-03 20:07:48 +0300

    created basic api functions for products and shops

--- app/_lib/apiServices.js
+++ app/_lib/apiServices.js
@@ -1,46 +1,67 @@
-// a file for all api requests to the json server
+// api services for supabase
+import supabase, { supabaseUrl } from "./supabase";
 
-// API request to get all shops
+const shopImageUrl = `${supabaseUrl}/storage/v1/object/public/shop/`;
+const productImageUrl = `${supabaseUrl}/storage/v1/object/public/product/`;
+
+// 1.api function for selecting all shops
 export const getAllShops = async () => {
-  const res = await fetch("http://localhost:8001/shops");
-  const data = await res.json();
+  const { data, error } = await supabase.from("shops").select("*");
+  if (error)
+    throw new Error(`Shop details could not be fetched: ${error.message}`);
   return data;
 };
 
-// api function to add a new shop
+// 2.api function for adding a new shop
 export const addShop = async (shop) => {
-  const res = await fetch("http://localhost:8001/shops", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(shop),
-  });
-
-  const data = await res.json();
+  const { data, error } = await supabase
+    .from("shops")
+    .insert([{ ...shop, logo: shop.shopImageUrl }]);
+  if (error) throw new Error(error.message);
   return data;
 };
 
-// api function to update a shop
+// 3.api function for updating a shop
 export const updateShop = async (shop) => {
-  const res = await fetch(`http://localhost:8001/shops/${shop.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(shop),
-  });
-
-  const data = await res.json();
+  const { data, error } = await supabase
+    .from("shops")
+    .update(shop)
+    .eq({ id: shop.id });
+  if (error) throw new Error(error.message);
   return data;
 };
 
-// api function to delete a shop
+// 4.api function for deleting a shop
 export const deleteShop = async (id) => {
-  const res = await fetch(`http://localhost:8001/shops/${id}`, {
-    method: "DELETE",
-  });
+  const { data, error } = await supabase.from("shops").delete().eq({ id });
+  if (error) throw new Error(error.message);
+  return data;
+};
 
-  const data = await res.json();
+// 1.api function for selecting all products
+export const getAllProducts = async () => {
+  const { data, error } = await supabase.from("products").select("*");
+  if (error) throw new Error(error.message);
+  return data;
+};
+// 2.api function for adding a new product
+export const addProduct = async (product) => {
+  const { data, error } = await supabase.from("products").insert([product]);
+  if (error) throw new Error(error.message);
+  return data;
+};
+// 3.api function for updating a product
+export const updateProduct = async (product) => {
+  const { data, error } = await supabase
+    .from("products")
+    .update(product)
+    .eq({ id: product.id });
+  if (error) throw new Error(error.message);
+  return data;
+};
+// 4.api function for deleting a product
+export const deleteProduct = async (id) => {
+  const { data, error } = await supabase.from("products").delete().eq({ id });
+  if (error) throw new Error(error.message);
   return data;
 };
