49fbf685a56472d1047c492bac0c2b7f3d00d704
MwauStephen <stephenmwau41@gmail.com>
2025-01-04 06:09:32 +0300

    passed close handler function to close modal on successful submission and on clicking close button

--- components/_shop/ShopForm.js
+++ components/_shop/ShopForm.js
@@ -3,35 +3,75 @@ import { Box, Card, Flex, Input, Text, Textarea } from "@chakra-ui/react";
 import { Field } from "../ui/field";
 import UploadButton from "../UploadButton";
 import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { useAddShop } from "@/app/_hooks/ShopHooks";
 
-const ShopForm = () => {
+const ShopForm = ({ shopDetailsToEdit = {}, handleClose }) => {
+  const { id: editId, ...editValues } = shopDetailsToEdit;
+  const isEditSession = Boolean(editId);
+  const { addNewShop } = useAddShop();
+
+  const { register, formState, reset, handleSubmit } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+
+  // handle form submission
+  const handleOnSubmit = async (data) => {
+    const image = typeof data.logo === "string" ? data.logo : data.logo[0];
+    console.log(data, "data from form");
+    addNewShop(
+      { ...data, logo: image },
+      {
+        onSuccess: () => {
+          reset();
+          handleClose();
+        },
+      }
+    );
+  };
+
   return (
     <Box>
-      <Flex as="form" flexDirection={{ base: "column", md: "column" }} gap={4}>
+      <Flex
+        as="form"
+        flexDirection={{ base: "column", md: "column" }}
+        gap={4}
+        onSubmit={handleSubmit(handleOnSubmit)}
+      >
         <Field label="Shop Name" required>
-          <Input placeholder="name of shop" />
+          <Input placeholder="name of shop" {...register("shopName")} />
         </Field>
         <Field label="Shop Description" required>
-          <Textarea placeholder="Your tech paradise" />
+          <Textarea
+            placeholder="Your tech paradise"
+            {...register("shopDescription")}
+          />
         </Field>
         <Field required label="Shop Logo">
-          <UploadButton />
+          <UploadButton {...register("logo")} />
         </Field>
         <Box textAlign={{ base: "center", md: "right" }}>
-          <Button textAlign="left" variant="outline" type="reset" mr={4}>
-            Cancel
+          <Button
+            textAlign="left"
+            variant="outline"
+            // type="reset"
+            mr={4}
+            key="close"
+            onClick={handleClose}
+          >
+            Close
           </Button>
           <Button
             textAlign="left"
             bgGradient="to-r"
             gradientFrom="#0da487"
             gradientTo="#20c997"
+            type="submit"
           >
             Submit
           </Button>
         </Box>
       </Flex>
-      {/* </Card.Root> */}
     </Box>
   );
 };
