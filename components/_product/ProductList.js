5c74a3cad9cf1fac36d7cde3edc88679dfe9f731
MwauStephen <stephenmwau41@gmail.com>
2025-01-05 22:09:50 +0300

    displayed empty component incase no products present

--- components/_product/ProductList.js
+++ components/_product/ProductList.js
@@ -14,6 +14,9 @@ import Pagination from "../Pagination";
 import Image from "next/image";
 import ProductForm from "./ProductForm";
 import ViewCard from "../ViewCard";
+import { useDeleteProduct, useFetchProducts } from "@/app/_hooks/ProductsHooks";
+import Empty from "../Empty";
+import Link from "next/link";
 
 const items = [
   {
@@ -67,6 +70,17 @@ const items = [
 ];
 
 const ProductList = () => {
+  const { products } = useFetchProducts();
+  const { deleteProduct } = useDeleteProduct();
+
+  if (products?.length === 0)
+    return (
+      <Empty
+        title="No products found.Please add a product."
+        action="Add Product"
+        path="products/create"
+      />
+    );
   return (
     <Card.Root p={4} boxShadow="xl">
       <Box textAlign="end" mb={4}>
@@ -107,27 +121,27 @@ const ProductList = () => {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {items.map((item, index) => (
-              <Table.Row key={item.id}>
+            {products?.map((product, index) => (
+              <Table.Row key={product.id}>
                 <Table.Cell>{index + 1}</Table.Cell>
-                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>{product.productName}</Table.Cell>
                 <Table.Cell
                   maxWidth="300px"
                   whiteSpace="nowrap"
                   overflow="hidden"
                   textOverflow="ellipsis"
                 >
-                  {item.description}
+                  {product.productDescription}
                 </Table.Cell>
-                <Table.Cell>{item.price}</Table.Cell>
-                <Table.Cell textAlign="end">{item.stock}</Table.Cell>
+                <Table.Cell>{product.price}</Table.Cell>
+                <Table.Cell textAlign="end">{product.stockLevel}</Table.Cell>
                 <Table.Cell textAlign="end">
                   <Flex justifyContent="flex-end">
                     <Image
-                      src={item.image}
+                      src={product.image}
                       width={40}
                       height={40}
-                      alt={item.name}
+                      alt={product.productName}
                     />
                   </Flex>
                 </Table.Cell>
@@ -149,17 +163,17 @@ const ProductList = () => {
                       <Stack spacing={4}>
                         <Text>
                           <Image
-                            src={item.logo}
+                            src={product.image}
                             width={400}
                             height={400}
-                            alt={item.name}
+                            alt={product.productName}
                           />
                         </Text>
                         <Text>
-                          <b>Product Name:</b> {item.name}
+                          <b>Product Name:</b> {product.productName}
                         </Text>
                         <Text>
-                          <b>Description: </b> {item.description}
+                          <b>Description: </b> {product.productDescription}
                         </Text>
                       </Stack>
                     </ViewCard>
@@ -170,9 +184,9 @@ const ProductList = () => {
                           <FaPencil size="1px" />
                         </IconButton>
                       }
-                      title={`Edit ${item.name}`}
+                      title={`Edit ${product.productName}`}
                     >
-                      <ProductForm />
+                      <ProductForm productDetailsToEdit={product} />
                     </ViewCard>
 
                     <ViewCard
@@ -181,21 +195,26 @@ const ProductList = () => {
                           <FaTrash size="1px" />
                         </IconButton>
                       }
-                      title={`Delete ${item.name} Product`}
+                      title={`Delete ${product.productName} Product`}
                       footerActions={[
-                        <Flex gap={2} key={item.id}>
+                        <Flex gap={2} key={product.id}>
                           <Button key="close" variant="outline">
                             Close
                           </Button>
-                          <Button key="close" colorPalette="red">
+                          <Button
+                            key={product.id}
+                            colorPalette="red"
+                            onClick={() => [deleteProduct(product.id)]}
+                          >
                             Delete
                           </Button>
                         </Flex>,
                       ]}
                     >
                       <Text>
-                        Are you sure you want to delete {` ${item.name}`}?This
-                        action cannot be undone.
+                        Are you sure you want to delete{" "}
+                        {` ${product.productName}`}?This action cannot be
+                        undone.
                       </Text>
                     </ViewCard>
                   </Flex>
