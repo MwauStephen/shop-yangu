a6bf7b3a567074bc91a85f0f6ea7d8d3a13f5c84
MwauStephen <stephenmwau41@gmail.com>
2025-01-05 17:48:39 +0300

    wired form for submission correctly

--- components/_product/ProductForm.js
+++ components/_product/ProductForm.js
@@ -1,27 +1,49 @@
-"use client";
+// "use client";
 import { Box, Card, Flex, Input, Text, Textarea } from "@chakra-ui/react";
 import { Field } from "../ui/field";
 import { Button } from "../ui/button";
 import UploadButton from "../UploadButton";
+import SelectContainer from "../SelectContainer";
+import { useFetchShops } from "@/app/_hooks/ShopHooks";
+import { useForm } from "react-hook-form";
 
 const ProductForm = () => {
+  const { shops } = useFetchShops();
+  const { register, formState, reset, handleSubmit } = useForm();
+
+  // handle form submission
+  const handleOnSubmit = (data) => {
+    console.log(data, "data from form");
+  };
+
   return (
-    <Box>
-      <Flex as="form" flexDirection={{ base: "column", md: "column" }} gap={4}>
+    <Box as="form" onSubmit={handleSubmit(handleOnSubmit)}>
+      <Flex flexDirection={{ base: "column", md: "column" }} gap={4}>
         <Field label="Product Name" required>
-          <Input placeholder="name of product" />
+          <Input placeholder="name of product" {...register("productName")} />
         </Field>
         <Field label="Product Price" required>
-          <Input placeholder="Ksh 100" />
+          <Input placeholder="Ksh 100" {...register("price")} />
         </Field>
         <Field label="Stock Level" required>
-          <Input placeholder="30" />
+          <Input placeholder="30" {...register("stockLevel")} />
         </Field>
         <Field label="Product Description" required>
-          <Textarea placeholder="Latest model with high-end features" />
+          <Textarea
+            placeholder="Latest model with high-end features"
+            {...register("productDescription")}
+          />
+        </Field>
+        <Field label="Select Shop">
+          <SelectContainer
+            title="Select A Shop"
+            placeholder="select shop"
+            data={shops}
+            register={register("shopName")}
+          />
         </Field>
         <Field required label="Product Image">
-          <UploadButton />
+          <UploadButton {...register("image")} />
         </Field>
         <Box textAlign={{ base: "center", md: "right" }}>
           <Button textAlign="left" variant="outline" type="reset" mr={4}>
@@ -32,6 +54,7 @@ const ProductForm = () => {
             bgGradient="to-r"
             gradientFrom="#0da487"
             gradientTo="#20c997"
+            type="submit"
           >
             Submit
           </Button>
