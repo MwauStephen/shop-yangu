2133e718a57d42616d91fe77416c840b19f84b73
MwauStephen <stephenmwau41@gmail.com>
2025-01-05 17:33:14 +0300

    passed down data as props and  finished styling  native select component

---
+++ components/SelectContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SelectContainer = ({ data }) => {
+  return (
+    <select
+      style={{
+        width: "100%",
+        padding: "0.5rem",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        fontSize: "0.8rem",
+      }}
+    >
+      {data.map((item, index) => {
+        return (
+          <option
+            value={item.shopName}
+            key={index}
+            style={{
+              padding: "0.5rem",
+            }}
+          >
+            {item.shopName}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default SelectContainer;
