baa6524f819e02188db4425140d9f2e572f64735
MwauStephen <stephenmwau41@gmail.com>
2025-01-05 13:12:15 +0300

    fixed an error on delete shop api function

--- app/_lib/apiServices.js
+++ app/_lib/apiServices.js
@@ -8,55 +8,200 @@ const productImageUrl = `${supabaseUrl}/storage/v1/object/public/product/`;
 export const getAllShops = async () => {
   const { data, error } = await supabase.from("shops").select("*");
   if (error)
-    throw new Error(`Shop details could not be fetched: ${error.message}`);
+    throw new Error(`Shop details could not be fetched.${error.message}`);
   return data;
 };
 
 // 2.api function for adding a new shop
 export const addShop = async (shop) => {
-  const { data, error } = await supabase
+  // Checks if image is present in supabase upon adding
+  const hasImagePath = shop.logo?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${shop.logo.name}`.replace(" /", "");
+  const imagePath = hasImagePath
+    ? shop.logo
+    : `${supabaseUrl}/storage/v1/object/public/shops/${imageName}`;
+
+  // 1.create a shop
+  let query = supabase.from("shops");
+
+  // 1a) Creates new shop
+  query = query.insert([{ ...shop, logo: imagePath }]);
+
+  const { data, error } = await query.select().single();
+
+  console.log(data, "data from api function");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Shop could not be created");
+  }
+
+  // If image has already a path i.e image has been uploaded  return data
+  if (hasImagePath) return data;
+
+  // 2.Upload the cabin image
+  const { error: storageError } = await supabase.storage
     .from("shops")
-    .insert([{ ...shop, logo: shop.shopImageUrl }]);
-  if (error) throw new Error(error.message);
+    .upload(imageName, shop.logo);
+
+  // 3.Prevent a new shop from being created if there is an error uploading the image(Delete the shop)
+  if (storageError) {
+    await supabase.from("shops").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Shop image could not be uploaded and shop was not created"
+    );
+  }
   return data;
 };
 
 // 3.api function for updating a shop
-export const updateShop = async (shop) => {
-  const { data, error } = await supabase
+export const updateShop = async (shop, id) => {
+  // Checks if image is present in supabase upon editing
+  const hasImagePath = shop.logo?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${shop.logo.name}`.replace(" /", "");
+  const imagePath = hasImagePath
+    ? shop.logo
+    : `${supabaseUrl}/storage/v1/object/public/shops/${imageName}`;
+
+  let query = supabase.from("shops");
+
+  // 1b) edits shop
+  if (id) {
+    query = query
+      .update({ ...shop, logo: imagePath })
+      .eq("id", id)
+      .select();
+  }
+
+  const { data, error } = await query.select();
+
+  // console.log(data, "data from api function");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Shop could not be updated");
+  }
+
+  // If image has already a path i.e image has been uploaded  return data
+  if (hasImagePath) return data;
+
+  // 2.Upload the cabin image
+  const { error: storageError } = await supabase.storage
     .from("shops")
-    .update(shop)
-    .eq({ id: shop.id });
-  if (error) throw new Error(error.message);
+    .upload(imageName, shop.logo);
+
+  // 3.Prevent a new cabin from being created if there is an error uploading the image(Delete the cabin)
+  if (storageError) {
+    await supabase.from("shops").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Shop image could not be uploaded and shop was not created"
+    );
+  }
   return data;
 };
-
 // 4.api function for deleting a shop
+
 export const deleteShop = async (id) => {
-  const { data, error } = await supabase.from("shops").delete().eq({ id });
-  if (error) throw new Error(error.message);
+  const { data, error } = await supabase.from("shops").delete().eq("id", id);
+  if (error) throw new Error(`Shop could not be deleted.${error.message}`);
   return data;
 };
 
 // 1.api function for selecting all products
 export const getAllProducts = async () => {
   const { data, error } = await supabase.from("products").select("*");
-  if (error) throw new Error(error.message);
+  if (error)
+    throw new Error(`Product details couldn't be fetched.${error.message}`);
   return data;
 };
 // 2.api function for adding a new product
 export const addProduct = async (product) => {
-  const { data, error } = await supabase.from("products").insert([product]);
-  if (error) throw new Error(error.message);
+  // Checks if image is present in supabase upon adding
+  const hasImagePath = product.image?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${product.image.name}`.replace(" /", "");
+  const imagePath = hasImagePath
+    ? product.image
+    : `${supabaseUrl}/storage/v1/object/public/products/${imageName}`;
+
+  // 1.create a shop
+  let query = supabase.from("products");
+
+  // 1a) Creates new shop
+  query = query.insert([{ ...product, image: imagePath }]);
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.log(error);
+    throw new Error("Products could not be created");
+  }
+
+  // If image has already a path i.e image has been uploaded  return data
+  if (hasImagePath) return data;
+
+  // 2.Upload the product image
+  const { error: storageError } = await supabase.storage
+    .from("products")
+    .upload(imageName, product.image);
+
+  // 3.Prevent a new product from being created if there is an error uploading the image(Delete the shop)
+  if (storageError) {
+    await supabase.from("products").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Product image could not be uploaded and product was not created"
+    );
+  }
   return data;
 };
 // 3.api function for updating a product
-export const updateProduct = async (product) => {
-  const { data, error } = await supabase
+export const updateProduct = async (product, id) => {
+  // Checks if image is present in supabase upon editing
+  const hasImagePath = product.image?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${product.image.name}`.replace(" /", "");
+  const imagePath = hasImagePath
+    ? product.image
+    : `${supabaseUrl}/storage/v1/object/public/products/${imageName}`;
+
+  let query = supabase.from("products");
+
+  // 1b) edits shop
+  if (id) {
+    query = query
+      .update({ ...product, image: imagePath })
+      .eq("id", id)
+      .select();
+  }
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.log(error);
+    throw new Error("Product could not be updated");
+  }
+
+  // If image has already a path i.e image has been uploaded  return data
+  if (hasImagePath) return data;
+
+  // 2.Upload the cabin image
+  const { error: storageError } = await supabase.storage
     .from("products")
-    .update(product)
-    .eq({ id: product.id });
-  if (error) throw new Error(error.message);
+    .upload(imageName, product.image);
+
+  // 3.Prevent a new product from being created if there is an error uploading the image(Delete the product)
+  if (storageError) {
+    await supabase.from("products").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Product image could not be uploaded and product was not created"
+    );
+  }
   return data;
 };
 // 4.api function for deleting a product
