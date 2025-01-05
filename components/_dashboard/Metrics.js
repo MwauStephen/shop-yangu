c33fab96aaa313cdcaafe0c707d6e5cf2543efc6
MwauStephen <stephenmwau41@gmail.com>
2025-01-06 01:22:05 +0300

    displayed stats metrics

--- components/_dashboard/Metrics.js
+++ components/_dashboard/Metrics.js
@@ -1,21 +1,46 @@
 import { Card, CardBody, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import StatsCard from "../StatsCard";
 import { FaShop, FaDollarSign, FaBuffer, FaChartLine } from "react-icons/fa6";
+import { useFetchShops } from "@/app/_hooks/ShopHooks";
+import {
+  useFetchProducts,
+  useProductSummary,
+} from "@/app/_hooks/ProductsHooks";
+import { getProductSummary } from "@/app/_lib/apiServices";
+import { formatCurrency } from "@/app/_utils/helpers";
 
 const Metrics = () => {
+  const { count: shopCount } = useFetchShops();
+  const { count: productCount } = useFetchProducts();
+
+  const { totalValue, totalStockLevel } = useProductSummary();
+
+  console.log(totalValue, totalStockLevel, "total value and stock level");
   return (
     <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={4}>
       <GridItem>
-        <StatsCard title="Total Shops" value="100" icon={<FaShop />} />
+        <StatsCard title="Total Shops" value={shopCount} icon={<FaShop />} />
       </GridItem>
       <GridItem>
-        <StatsCard title="Total Products" value="100" icon={<FaBuffer />} />
+        <StatsCard
+          title="Total Products"
+          value={productCount}
+          icon={<FaBuffer />}
+        />
       </GridItem>
       <GridItem>
-        <StatsCard title="Total Value" value="50" icon={<FaDollarSign />} />
+        <StatsCard
+          title="Total Value"
+          value={formatCurrency(totalValue)}
+          icon={<FaDollarSign />}
+        />
       </GridItem>
       <GridItem>
-        <StatsCard title="Total Stock" value="200" icon={<FaChartLine />} />
+        <StatsCard
+          title="Total Stock"
+          value={totalStockLevel}
+          icon={<FaChartLine />}
+        />
       </GridItem>
     </Grid>
   );
