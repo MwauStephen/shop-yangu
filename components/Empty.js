c61d3276155458fe1c165bb7a6b2096b34a921f5
MwauStephen <stephenmwau41@gmail.com>
2025-01-05 22:06:16 +0300

    finished creating this component

--- components/Empty.js
+++ components/Empty.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { EmptyState } from "./ui/empty-state";
+import Link from "next/link";
 
-const Empty = ({ icon, title, description }) => {
-  return <EmptyState icon={icon} title={title} description={description} />;
+const Empty = ({ icon, title, description, action, path }) => {
+  return (
+    <EmptyState icon={icon} title={title} description={description}>
+      <Link
+        href={path}
+        style={{ textDecoration: "underline", color: "#20c997" }}
+      >
+        {action}
+      </Link>
+    </EmptyState>
+  );
 };
 
 export default Empty;
