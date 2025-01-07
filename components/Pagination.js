a24b58c75f93f21fb20b726c93fbfc5b14ddb836
MwauStephen <stephenmwau41@gmail.com>
2025-01-07 12:20:40 +0300

    passed props into pagination component and handle pagination correctly

--- components/Pagination.js
+++ components/Pagination.js
@@ -8,19 +8,32 @@ import {
 } from "@/components/ui/pagination";
 import { HStack, Flex } from "@chakra-ui/react";
 
-const Pagination = () => {
+const Pagination = ({ count, pageSize, currentPage, onPageChange }) => {
+  const totalPages = Math.ceil(count / pageSize); // 10/5 =2 pages
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
   return (
     <PaginationRoot
-      count={50}
-      pageSize={5}
-      defaultPage={1}
+      count={count}
+      pageSize={pageSize}
+      currentPage={currentPage}
       maxW="240px"
       mx="auto"
     >
       <HStack justify="flex-end">
         <PaginationPageText format="long" flex="1" />
-        <PaginationPrevTrigger />
-        <PaginationNextTrigger />
+        <PaginationPrevTrigger
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        />
+        <PaginationNextTrigger
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        />
       </HStack>
     </PaginationRoot>
   );
