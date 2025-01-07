caf4e4c63cd27dc0bbd94e64a1c8badaa06d25ff
MwauStephen <stephenmwau41@gmail.com>
2025-01-07 12:17:16 +0300

    modified hook to handle pagination

--- app/_hooks/ProductsHooks.js
+++ app/_hooks/ProductsHooks.js
@@ -7,19 +7,21 @@ import {
   getStockStatus,
 } from "../_lib/apiServices";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 // all products custom hooks
-export const useFetchProducts = () => {
+export const useFetchProducts = (page) => {
+  const [currentPage, setCurrentPage] = useState(page);
   const {
     data: { data: products, count } = {},
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["products"],
-    queryFn: () => getAllProducts(),
+    queryKey: ["products", currentPage],
+    queryFn: () => getAllProducts(currentPage),
   });
-  return { products, error, isLoading, count };
+  return { products, error, isLoading, count, currentPage, setCurrentPage };
 };
 export const useAddProduct = () => {
   const queryClient = useQueryClient();
@@ -66,12 +68,15 @@ export const useDeleteProduct = () => {
 };
 
 export const useProductSummary = () => {
-  const { data = {}, error } = useQuery({
+  const { data, error } = useQuery({
     queryKey: ["productSummary"],
-    queryFn: () => getProductSummary(),
+    queryFn: getProductSummary,
   });
-  const { totalValue = 0, totalStockLevel = 0 } = data;
-  return { totalValue, totalStockLevel, error };
+  // console.log(data);
+  // const { totalValue, totalStockLevel } = data;
+  // const { totalValue = 0, totalStockLevel = 0 } = data;
+  // return { totalValue, totalStockLevel, error };
+  return { data, error };
 };
 
 export const useFetchStockStatus = () => {
