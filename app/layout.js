23abc21bebc564b0bd99960561b4e1dd8f896b6a
MwauStephen <stephenmwau41@gmail.com>
2025-01-09 11:48:55 +0300

    prtotected the roiutes and access to main page by ensuring login page is the first rendered page

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
+"use client"; // Mark as Client Component
+
 import React from "react";
 import { Provider } from "@/components/ui/provider";
 import DashboardLayout from "@/components/DashboardLayout";
 import { Roboto } from "next/font/google";
 import QueryProvider from "@/components/QueryProvider";
 import ToastContainer from "@/components/ToastContainer";
+import { usePathname } from "next/navigation";
+import { metadata } from "@/metadata";
 
 // loading fonts
 const roboto = Roboto({
@@ -12,19 +16,29 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Shop Yangu | E-commerce",
-  description: "An E-commerce admin dashboard",
-};
-
 const RootLayout = ({ children }) => {
+  const pathname = usePathname();
+
+  // Define paths that should not include the DashboardLayout
+  const noLayoutPaths = ["/login", "/logo"];
+
+  const shouldUseDashboardLayout = !noLayoutPaths.includes(pathname);
+
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body className={roboto.className}>
         <Provider>
           <ToastContainer />
           <QueryProvider>
-            <DashboardLayout>{children}</DashboardLayout>
+            {shouldUseDashboardLayout ? (
+              <DashboardLayout>{children}</DashboardLayout>
+            ) : (
+              children
+            )}
           </QueryProvider>
         </Provider>
       </body>
