a59d36af5f469570db716ac9fee92288a53ceb97
MwauStephen <stephenmwau41@gmail.com>
2025-01-09 11:48:13 +0300

    added initial app layout into dump code

--- dump.js
+++ dump.js
@@ -208,3 +208,39 @@
 //     throw new Error(`Product details couldn't be fetched.${error.message}`);
 //   return { data, count };
 // };
+
+// import React from "react";
+// import { Provider } from "@/components/ui/provider";
+// import DashboardLayout from "@/components/DashboardLayout";
+// import { Roboto } from "next/font/google";
+// import QueryProvider from "@/components/QueryProvider";
+// import ToastContainer from "@/components/ToastContainer";
+
+// // loading fonts
+// const roboto = Roboto({
+//   display: "swap",
+//   weight: "400",
+//   subsets: ["latin"],
+// });
+
+// export const metadata = {
+//   title: "Shop Yangu | E-commerce",
+//   description: "An E-commerce admin dashboard",
+// };
+
+// const RootLayout = ({ children }) => {
+//   return (
+//     <html lang="en">
+//       <body className={roboto.className}>
+//         <Provider>
+//           <ToastContainer />
+//           <QueryProvider>
+//             <DashboardLayout>{children}</DashboardLayout>
+//           </QueryProvider>
+//         </Provider>
+//       </body>
+//     </html>
+//   );
+// };
+
+// export default RootLayout;
